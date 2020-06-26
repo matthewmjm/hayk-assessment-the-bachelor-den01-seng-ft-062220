cddeb068752a7759ea67bc9db2a8a0c633f8e823
matthewmjm <matthewmjm@yahoo.com>
2020-06-25 22:54:01 -0600

    passed all 15 tests

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,93 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner = ""
+  season_identified = data[season]
+  season_identified.each do |contestant|
+    if contestant["status"] == "Winner"
+      winner = contestant["name"].split.first
+    else
+    end
+  end
+  winner
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  #collapse all contestants into one array of 290 contestants
+  all_contestants = []
+  data.each do |season|
+    season_contestants = []
+      season.each do |castmember|
+        season_contestants = season_contestants << castmember
+      end
+      season_contestants.shift
+      all_contestants << season_contestants
+    end
+    all_contestants= all_contestants.flatten
+  all_contestants
+  #find the contestant that has the occupation passed into the objects
+  all_contestants.each do |contestant|
+    if contestant["occupation"] == occupation
+      return contestant["name"]
+    else
+    end
+  end
+
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  #collapse all contestants into one array of 290 contestants
+  all_contestants = []
+  data.each do |season|
+    season_contestants = []
+      season.each do |castmember|
+        season_contestants = season_contestants << castmember
+      end
+      season_contestants.shift
+      all_contestants << season_contestants
+    end
+    all_contestants= all_contestants.flatten
+  all_contestants
+  #find the contestant that has the occupation passed into the objects
+  counter = 0
+  all_contestants.each do |contestant|
+    if contestant["hometown"] == hometown
+      counter += 1
+    else
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  #collapse all contestants into one array of 290 contestants
+  all_contestants = []
+  data.each do |season|
+    season_contestants = []
+      season.each do |castmember|
+        season_contestants = season_contestants << castmember
+      end
+      season_contestants.shift
+      all_contestants << season_contestants
+    end
+    all_contestants= all_contestants.flatten
+  all_contestants
+  #find the contestant that has the occupation passed into the objects
+  all_contestants.find do |contestant|
+    if contestant["hometown"] == hometown
+      return contestant["occupation"]
+    else
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  average = 0.0
+  total_ages = 0.0
+  ages = []
+  season_identified = data[season]
+  season_identified.map { |contestant| ages << contestant["age"].to_f }
+  total_ages = ages.reduce(0) { |sum, num| sum + num}
+  average = (total_ages/(ages.length)).round
+  average
 end
